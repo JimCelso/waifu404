16b6c8c0de2243c48afeea2c8a77a34424194d21
Jiménez de los Santos Celso Andrés <celso@JimCelso.com>
2025-08-03 21:10:52 -0600

    segunda actualización de archivos

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
 const intro = document.getElementById('intro');
-const main = document.getElementById('main');
+const main = document.getElementById('mainContent');
 const sound = document.getElementById('startSound');
 const btn = document.getElementById('enterBtn');
+const glitchTitle = document.getElementById('glitchTitle');
 
-// Mostrar botón después de 3 segundos
-setTimeout(() => {
-  btn.classList.remove('hidden');
-  btn.classList.add('show');
-}, 3000);
-
-// Clic en botón
 btn.addEventListener('click', () => {
-  sound.currentTime = 0;
   sound.play();
-
   intro.style.opacity = '0';
-
   setTimeout(() => {
     intro.style.display = 'none';
     main.style.display = 'block';
     document.body.style.overflow = 'auto';
   }, 1000);
 });
+
+// Glitch intermitente del título
+setInterval(() => {
+  glitchTitle.classList.remove('glitch');
+  void glitchTitle.offsetWidth;
+  glitchTitle.classList.add('glitch');
+}, 4000);
